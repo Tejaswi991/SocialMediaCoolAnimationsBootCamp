ed5d622d31a2f3df847db6065043af1653894e12
Satya Dodda <satya.tejaswi99@gmail.com>
2024-02-15 11:43:40 +0530

    Initial Commit

---
+++ SocialMediaCoolAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialMediaCoolAnimations
+//
+//  Created by C7K6SW on 15/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialMediaCoolAnimations/SocialMediaCoolAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaCoolAnimationsApp.swift
+//  SocialMediaCoolAnimations
+//
+//  Created by C7K6SW on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaCoolAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
