15e816433cb603c0cf87630e2d8b7691fa7c8b74
Satya Dodda <satya.tejaswi99@gmail.com>
2024-02-16 18:15:39 +0530

    Youtube skip add

--- SocialMediaCoolAnimations/Youtube/SkipAdd.swift
+++ SocialMediaCoolAnimations/Youtube/SkipAdd.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct SkipAdd: View {
+    @State var seconds = 5
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack(spacing:0){
+            if seconds>0{
+                Text("\(seconds)")
+                    .foregroundStyle(.white)
+                    .frame(width:40,height:50)
+                    .background(.black.opacity(0.8))
+                
+                Image("girl")
+                    .resizable()
+                    .frame(width:80,height:50)
+            }
+            if seconds==0{
+                skipAdd()
+            }
+        }
+        .onAppear(perform: {
+            Timer.scheduledTimer(withTimeInterval:1, repeats:true) { timer in
+                seconds-=1
+                if seconds<1{
+                    seconds=0
+                    timer.invalidate()
+                }
+            }
+        })
+        .cornerRadius(10)
+        
+    }
+    func skipAdd() -> some View {
+        HStack(spacing:10){
+            Text("Skip")
+            HStack(spacing:5){
+                Image(systemName:"play.fill")
+                Rectangle().frame(width:1,height:20)
+                    .bold()
+            }
+        }
+        .foregroundColor(.white)
+        .padding()
+        .padding([.leading,.trailing])
+        .background(.black.opacity(0.8))
+        .cornerRadius(30)
+        
+        
     }
 }
 
 #Preview {
     SkipAdd()
+       // .preferredColorScheme(.dark)
 }
