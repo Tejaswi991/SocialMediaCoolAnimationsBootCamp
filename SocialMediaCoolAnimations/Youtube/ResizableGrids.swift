3fee5d5fbcc26d5130d1200e22b1fbdfde221e0e
Satya Dodda <satya.tejaswi99@gmail.com>
2024-02-16 18:54:58 +0530

    Added foldable grid view(As shorts displayed in yt)

---
+++ SocialMediaCoolAnimations/Youtube/ResizableGrids.swift
@@ -0,0 +1,18 @@
+//
+//  ResizableGrids.swift
+//  SocialMediaCoolAnimations
+//
+//  Created by C7K6SW on 16/02/24.
+//
+
+import SwiftUI
+
+struct ResizableGrids: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResizableGrids()
+}
