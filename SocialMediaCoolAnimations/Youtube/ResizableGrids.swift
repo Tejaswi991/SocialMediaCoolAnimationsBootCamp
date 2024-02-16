434cc04b727e14f4f9878acc810a11acf17835c3
Satya Dodda <satya.tejaswi99@gmail.com>
2024-02-16 18:55:59 +0530

    added foldable scrollview

--- SocialMediaCoolAnimations/Youtube/ResizableGrids.swift
+++ SocialMediaCoolAnimations/Youtube/ResizableGrids.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ResizableGrids: View {
+    let columns = [GridItem(.fixed(100), spacing:30, alignment: .center),GridItem(.fixed(100), spacing:30, alignment: .center),
+                   GridItem(.fixed(100), spacing:30, alignment: .center)]
+    @State var showMore=false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical){
+            LazyVGrid(columns: columns, content: {
+                
+                Image(.girl).resizable().frame(width:100, height:100)
+                Image(.girl).resizable().frame(width:100, height:100)
+                Image(.girl).resizable().frame(width:100, height:100)
+                if showMore{
+                    Group{
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                        Image(.girl).resizable().frame(width:100, height:100)
+                    }
+                }
+                
+            })
+            Button(action: {
+                withAnimation(.easeInOut(duration:0.5)){
+                    showMore.toggle()
+                }
+               
+            }, label: {
+                Text(showMore ? "Show Less" : "Show More")
+            })
+        }
     }
 }
 
