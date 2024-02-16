6ce4fad35165005a308d49fdcf7bdc44f13d45ab
Satya Dodda <satya.tejaswi99@gmail.com>
2024-02-16 18:14:50 +0530

    Youtube skip add button

---
+++ SocialMediaCoolAnimations/Youtube/SkipAdd.swift
@@ -0,0 +1,18 @@
+//
+//  SkipAdd.swift
+//  SocialMediaCoolAnimations
+//
+//  Created by C7K6SW on 16/02/24.
+//
+
+import SwiftUI
+
+struct SkipAdd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkipAdd()
+}
